e32b43762866fa5fd26e69610816e3d9c2d6836a
Ted Summer <ted.summer2@gmail.com>
2018-08-21 14:10:28 -0500

    feat(async): allow async/await expressions

--- tests/lib/rules/no-actor-in-scenario.js
+++ tests/lib/rules/no-actor-in-scenario.js
@@ -4,13 +4,13 @@ var rule = require('../../../lib/rules/no-actor-in-scenario');
 var actorMethods = require('../../../lib/rules/actorMethods');
 var RuleTester = require('eslint').RuleTester;
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({"parserOptions": { "ecmaVersion": 8 }});
 
-function invalidScenarios(functionStyle) {
+function invalidScenarios(functionStyle, hasAwait) {
   var invalidScenarios = actorMethods.map(function(actorMethod) {
+    let awaitString = hasAwait ? "await" : "";
     return {
-      code: `Scenario('My scenario', ${functionStyle} { I.${actorMethod}() })`,
-      parserOptions: { ecmaVersion: 6 },
+      code: `Scenario('My scenario', ${functionStyle} { ${awaitString} I.${actorMethod}(); let x = ${awaitString} I.${actorMethod}(); });`,
       errors: [{
         message: "Do not use actor in Scenario, prefer delegating to page objects"
       }]
@@ -22,7 +22,9 @@ function invalidScenarios(functionStyle) {
 ruleTester.run("no-actor-in-scenario", rule, {
   valid: [
     "Before(function(I) { I.amOnPage() }); Scenario('My scenario', function(Page) { Page.submit() });",
+    "Before(function(I) { I.amOnPage() }); Scenario('My scenario', async function(Page) { await Page.submit() });"
   ],
-  invalid: invalidScenarios("function(I)").concat(invalidScenarios("(I) =>"))
+  invalid: invalidScenarios("function(I)").concat(invalidScenarios("async function(I)", true))
+    .concat(invalidScenarios("(I) =>")).concat(invalidScenarios("async (I) =>", true))
 });
 
