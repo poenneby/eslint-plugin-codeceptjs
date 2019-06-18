a4d39be58f1843836fc5b5ae2b43d690cac791b0
Jonathan Sharpe <jsharpe@pivotal.io>
2019-06-18 13:04:56 +0100

    New rule: no-exclusive-tests

---
+++ tests/lib/rules/no-exclusive-tests.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var RuleTester = require('eslint').RuleTester;
+
+var rule = require('../../../lib/rules/no-exclusive-tests');
+
+var ruleTester = new RuleTester();
+
+function invalidScenario (code) {
+  return {
+    code: code,
+    parserOptions: { ecmaVersion: 6 },
+    errors: [
+      { message: 'Unexpected exclusive test' }
+    ]
+  };
+}
+
+ruleTester.run('no-exclusive-tests', rule, {
+  valid: [
+    'Scenario("this is cool", function () {});'
+  ],
+  invalid: [
+    invalidScenario('Scenario.only("this is not", function () {});')
+  ],
+});
