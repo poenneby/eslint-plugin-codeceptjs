b92fd5917b5de38b0add197a9fd2ade0e5f41ea3
Kapil Pau <kapilpau@hotmail.com>
2019-11-15 15:30:55 +0000

    Added rule for finding pauses

---
+++ tests/lib/rules/no-pause-in-scenario.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var RuleTester = require('eslint').RuleTester;
+
+var rule = require('../../../lib/rules/no-pause-in-scenario');
+
+var ruleTester = new RuleTester();
+
+function invalidScenario(input, output) {
+  return {
+    code: input,
+    parserOptions: { ecmaVersion: 6 },
+    errors: [
+      { message: 'Unexpected pause in test' }
+    ],
+    output: output
+  };
+}
+
+ruleTester.run('no-pause-in-scenario', rule, {
+  valid: [
+    'Scenario("this is cool", function () {});'
+  ],
+  invalid: [
+    invalidScenario(
+        'Scenario("this is not", function () {pause()});',
+	'Scenario("this is not", function () {pause()});'
+    ),
+    invalidScenario(
+        'Scenario("nor is this", function () {pause()});',
+	'Scenario("nor is this", function () {pause()});'
+    )
+  ],
+});
